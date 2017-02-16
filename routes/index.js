12aa83469b94386515d471e532b87b56e32e6e8e
juliaconnly <jconnly@ucsd.edu>
2017-02-15 17:38:08 -0800

    nav bar is working

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,9 @@
+var data = require('../data.json');
 
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
-  res.render('index', {
-    'projects': [
-      { 'name': 'John Doe',
-        'image': 'head.jpg',
-        'id': 'project1'
-      },
-      { 'name': 'Jane Doe',
-        'image': 'head.jpg',
-        'id': 'project2'
-      },
-      { 'name': 'Kaila Lee',
-        'image': 'head.jpg',
-        'id': 'project3'
-      },
-      { 'name': 'Jenn Chan',
-        'image': 'head.jpg',
-        'id': 'project4'
-      },
-      { 'name': 'Julia Connly',
-        'image': 'head.jpg',
-        'id': 'project5'
-      },
-      { 'name': 'Kobe Bryant',
-        'image': 'head.jpg',
-        'id': 'project6'
-      },
-      { 'name': 'Ellen Degeneres',
-        'image': 'head.jpg',
-        'id': 'project7'
-      },
-      { 'name': 'Steph Curry',
-        'image': 'head.jpg',
-        'id': 'project8'
-      }
-    ]  
-  });
+  res.render('index', data);
 };
